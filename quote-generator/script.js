840fc84529d9aab96e3086dcf73bc2edf0360155
n8fury <mhjoy547@yahoo.com>
2022-03-18 18:34:46 +0600

    modified DOM

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -1,8 +1,21 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const quoteAuthor = document.getElementById("author");
+const twitterButton = document.getElementById("twitter-button");
+const facebookButton = document.getElementById("facebook-button");
+const newQuoteButton = document.getElementById("new-quote");
+
 let quote = [];
 
 function newQuote() {
   const randomQuote = quote[Math.floor(Math.random() * quote.length)];
-  console.log(randomQuote);
+  quoteText.textContent = randomQuote.text;
+  if (!randomQuote.author) {
+    quoteAuthor.textContent = "Unknown";
+  } else {
+    quoteAuthor.textContent = randomQuote.author;
+  }
+
 }
 
 function localQuote() {
