810066fbd9c277fd2a856bd6f7f1bfd59c37bdf0
n8fury <mhjoy547@yahoo.com>
2022-03-19 02:55:04 +0600

    completed twitter share button and removed facebook share

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -2,11 +2,10 @@ const quoteContainer = document.getElementById("quote-container");
 const quoteText = document.getElementById("quote");
 const quoteAuthor = document.getElementById("author");
 const twitterButton = document.getElementById("twitter-button");
-const facebookButton = document.getElementById("facebook-button");
 const newQuoteButton = document.getElementById("new-quote");
 
 let quote = [];
-
+// select random quote from array of quotes
 function newQuote() {
   const randomQuote = quote[Math.floor(Math.random() * quote.length)];
   quoteText.textContent = randomQuote.text;
@@ -15,23 +14,35 @@ function newQuote() {
   } else {
     quoteAuthor.textContent = randomQuote.author;
   }
-
 }
-
+// select random quote from local array of quotes
 function localQuote() {
   const localQuote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-  console.log(localQuote);
+  quoteText.textContent = localQuote.text;
+  if (!localQuote.author) {
+    quoteAuthor.textContent = "Unknown";
+  } else {
+    quoteAuthor.textContent = localQuote.author;
+  }
 }
-
+// fetching quotes from api
 async function getQuote() {
   try {
     const response = await fetch("https://type.fit/api/quotes");
     quote = await response.json();
     newQuote();
   } catch (error) {
-    // Do something for an error here
     localQuote();
   }
 }
+// post to twitter function
+function tweetQuote() {
+  const tweetUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${quoteAuthor.textContent}`;
+  window.open(tweetUrl, "_blank");
+}
+
+
 getQuote();
+// event_listeners
 newQuoteButton.addEventListener("click", newQuote);
+twitterButton.addEventListener("click", tweetQuote);
