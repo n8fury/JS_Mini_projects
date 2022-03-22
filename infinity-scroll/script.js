e152b3399ff35136f81f1f03efc9af76c95b716f
n8fury <mhjoy547@yahoo.com>
2022-03-23 00:54:35 +0600

    loaded images via API

--- infinity-scroll/script.js
+++ infinity-scroll/script.js
@@ -1,18 +1,34 @@
-const imageContainer = document.getElementById("photo-container");
+const photoContainer = document.getElementById("photo-container");
 const loader = document.getElementById("loader");
 
 let photosArray = [];
 // Api(UNSPLASH)
-const apiKey = 't_4J4GMWETZ55kdAKMuaQ9IYnazVh0UhzFaTX-RDWZQ';
-const count = 40;
+const apiKey = 'zk-c3koRKsWJveqTao84FGHFruXOaqN2BhbavpV5fzA';
+const count = 30;
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
-
-
+// display photos
+function displayPhotos(array) {
+  array.forEach((photo) => {
+    const anchor = document.createElement("a");
+    anchor.setAttribute("href", photo.links.html);
+    anchor.setAttribute("target", "_blank");
+    const img = document.createElement("img");
+    img.setAttribute("src", photo.urls.regular);
+    img.setAttribute("alt", photo.alt_description);
+    img.setAttribute("title", photo.alt_description);
+    anchor.appendChild(img);
+    photoContainer.appendChild(anchor);
+    console.log("working");
+  });
+}
+// Fetching data from the API
 async function getPhotos() {
   try {
     const res = await fetch(apiUrl);
     const photosArray = await res.json();
     console.log(photosArray);
+    displayPhotos(photosArray);
+    console.log("working_2");
   } catch (error) {
 
   }
