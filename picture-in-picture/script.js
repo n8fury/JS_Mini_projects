37d344320047faa4acb585435dfaccfe034e247a
Mamdud Hasan <mhjoy547@yahoo.com>
2022-04-03 23:51:55 +0600

    added mediastream selection

--- picture-in-picture/script.js
+++ picture-in-picture/script.js
@@ -1,2 +1,17 @@
 const video = document.getElementById('video');
 const video_button = document.getElementById("button");
+
+// purpose of this function is to wait for the media stream to pass video element and play it
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    video.srcObject = mediaStream;
+    video.onloadedmetadata = () => {
+      video.play();
+    }
+  } catch (error) {
+    console.log("error is ", error);
+  }
+}
+
+selectMediaStream();
